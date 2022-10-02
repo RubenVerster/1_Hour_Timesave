df2f889fcbb54ec24cf6fa5b3076edf0315006d3
verster <rrverster31415@gmail.com>
2022-10-02 08:59:32 +0200

    rtk implemented

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+import React from 'react';
+import type { RootState } from '../store';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from '../store/home';
+
 const Home: NextPage = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <Head>
@@ -14,6 +22,18 @@ const Home: NextPage = () => {
         <h1>NASA Tech Reports</h1>
 
         <div>Boilerplate app for faster development. Maybe look into Redwood as well</div>
+
+        <div>
+          <div>
+            <button aria-label='Increment value' onClick={() => dispatch(increment())}>
+              Increment
+            </button>
+            <span>{count}</span>
+            <button aria-label='Decrement value' onClick={() => dispatch(decrement())}>
+              Decrement
+            </button>
+          </div>
+        </div>
       </main>
 
       <footer>
